ed1b51a33f0a44aa8a29020c57bea60cbb4d0c00
Ruslan <ruslanguns@gmail.com>
2023-01-28 18:57:37 +0000

    Cambiar el nombre del dropplet

--- droplet_ejemplo.tf
+++ droplet_ejemplo.tf
@@ -1,14 +1,14 @@
 # Create a new Web Droplet in the nyc2 region
 resource "digitalocean_droplet" "prueba" {
   image  = "ubuntu-20-04-x64"
-  name   = "s-1vcpu-1gb-amd"
+  name   = "Prueba"
   region = "fra1"
   size   = "s-1vcpu-1gb"
 }
 
 output "digitalocean_droplet" {
   value = {
-    message = "Droplet creado"
+    message = "${digitalocean_droplet.prueba.name} Droplet creado"
     id      = digitalocean_droplet.prueba.id
     ipv4    = digitalocean_droplet.prueba.ipv4_address
   }
