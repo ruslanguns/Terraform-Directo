e88834402417442551f016ce76d6d0e0b075c88c
Ruslan <ruslanguns@gmail.com>
2023-01-28 19:05:21 +0000

    Bajando la capacidad - Prueba

--- droplet_ejemplo.tf
+++ droplet_ejemplo.tf
@@ -3,7 +3,7 @@ resource "digitalocean_droplet" "prueba" {
   image  = "ubuntu-20-04-x64"
   name   = "Prueba"
   region = "fra1"
-  size   = "s-1vcpu-2gb-amd"
+  size   = "s-1vcpu-1gb"
 }
 
 output "digitalocean_droplet" {
